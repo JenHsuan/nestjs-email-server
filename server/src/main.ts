f664b24d984a7ed8deb1c895faf8c44ad3da45bf
JenHsuan <ofalpha@gmail.com>
2022-02-13 15:26:12 +0800

    Add Cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,6 +12,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.useStaticAssets(join(__dirname, '..', 'views'));
